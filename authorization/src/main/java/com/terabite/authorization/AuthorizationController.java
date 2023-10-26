73f1c098c10c573fb1c6aa242c624188b861814b
Sanguine-Choleric <130211779+Sanguine-Choleric@users.noreply.github.com>
2023-10-26 12:50:30 -0700

    [ADAPTEDS-51] Added URI-based versioning mapping for endpoints

--- authorization/src/main/java/com/terabite/authorization/AuthorizationController.java
+++ authorization/src/main/java/com/terabite/authorization/AuthorizationController.java
@@ -2,37 +2,39 @@ package com.terabite.authorization;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/v1/user")
 public class AuthorizationController {
 
-    @GetMapping("/user/signup")
+    @GetMapping("/signup")
     public Payload userSignup() {
         return new Payload("Reached signup");
     }
 
-    @PostMapping("/user/signup")
+    @PostMapping("/signup")
     public Payload userSignupPost() {
         return new Payload("Reached signup POST");
     }
 
-    @GetMapping("/user/login")
+    @GetMapping("/login")
     public Payload userLogin() {
         return new Payload("Reached login");
     }
 
-    @PostMapping("/user/login")
+    @PostMapping("/login")
     public Payload userLoginPost() {
         return new Payload("Reached login POST");
     }
 
-    @GetMapping("/user/logout")
+    @GetMapping("/logout")
     public Payload userLogoutGet() {
         return new Payload("Reached logout");
     }
 
-    @PostMapping("/user/logout")
+    @PostMapping("/logout")
     public Payload userLogout() {
         return new Payload("Reached logout POST");
     }
