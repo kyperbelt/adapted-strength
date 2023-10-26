f8a566eb9ff17e4fcf3d536f3d1ad1eded411251
Sanguine-Choleric <130211779+Sanguine-Choleric@users.noreply.github.com>
2023-10-26 12:52:04 -0700

    [ADAPTEDS-51] Removed GET mappings

--- authorization/src/main/java/com/terabite/authorization/AuthorizationController.java
+++ authorization/src/main/java/com/terabite/authorization/AuthorizationController.java
@@ -1,6 +1,5 @@
 package com.terabite.authorization;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,29 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/user")
 public class AuthorizationController {
 
-    @GetMapping("/signup")
-    public Payload userSignup() {
-        return new Payload("Reached signup");
-    }
 
     @PostMapping("/signup")
     public Payload userSignupPost() {
         return new Payload("Reached signup POST");
     }
 
-    @GetMapping("/login")
-    public Payload userLogin() {
-        return new Payload("Reached login");
-    }
-
     @PostMapping("/login")
     public Payload userLoginPost() {
         return new Payload("Reached login POST");
-    }
-
-    @GetMapping("/logout")
-    public Payload userLogoutGet() {
-        return new Payload("Reached logout");
     }
 
     @PostMapping("/logout")
