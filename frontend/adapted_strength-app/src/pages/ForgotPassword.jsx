f3288dfb70f6cd657cdf54bfe53863d459717ac7
Jonathan Camarena <jonathan@DESKTOP-UUH7EJJ>
2023-10-30 17:24:05 -0700

    initial commit - testing with tailwind

--- frontend/adapted_strength-app/src/pages/ForgotPassword.jsx
+++ frontend/adapted_strength-app/src/pages/ForgotPassword.jsx
@@ -1,3 +1,23 @@
+function EmailField() {
+    return (<input type="email" placeholder="Email" />);
+}
+
+function SubmitButton() {
+    return (<button type="submit">Submit</button>);
+}
+
 export default function ForgotPassword() {
-    return (<h1>Forgot Password</h1>);
+    return (<div className="content-center w-full max-w-xs">
+        <h1 className="mx-0 text-center">Forgot Password</h1>
+        <div className="w-full max-w-xs">
+            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <div>
+                    <EmailField />
+                </div>
+                <div>
+                    <SubmitButton />
+                </div>
+            </form>
+        </div>
+    </div>);
 };
