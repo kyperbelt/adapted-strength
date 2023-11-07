2bbb0ec7994810f6bf9a0028e5d541e149786c47
CaseyCubed <casey.paras@yahoo.com>
2023-11-06 18:23:22 -0800

    Removing .js files that were not needed

--- frontend/adapted_strength-app/src/pages/Forgot_Pass.js
+++
@@ -1,7 +0,0 @@
-export const Forgot_Pass =() => {
-    return(
-        <h1>
-            Forgot Password page
-        </h1>
-    );
-};
--- frontend/adapted_strength-app/src/pages/Login_Page.js
+++
@@ -1,7 +0,0 @@
-export const Login_Page =() => {
-    return(
-        <h1>
-            Login Page
-        </h1>
-    );
-};
