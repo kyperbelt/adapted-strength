7f3fe75dbe7a7c4064117bfcff36ed380a845fda
Kyperbelt <kyperbelt@users.noreply.github.com>
2023-11-11 20:05:57 -0800

    [ADAPTEDS-81] Create Forgot Password Page (#21)
    * initial commit - testing with tailwind
    
    * [ADAPTEDS-81] Create Forgot Password Page
    
    initial commit - testing with tailwind
    
    working on page
    
    continued work on forgot password page
    
    [ADAPTEDS-81] Create Forgot Password Page
    
    ---------
    
    Co-authored-by: Jonathan Camarena <jonathan@DESKTOP-UUH7EJJ>

--- frontend/adapted_strength-app/src/App.js
+++ frontend/adapted_strength-app/src/App.js
@@ -3,42 +3,25 @@
 Module: App.js
 Team: TeraBITE
 */
-import logo from './logo.png';
-import icon from './ladyIcon.png'
 import './App.css';
-import NavBar from './components/navBar';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./pages/Layout";
+// routes imported from pages folder
+// They are still only react components
 import Home from "./pages/Home";
 import ForgotPassword from "./pages/ForgotPassword";
-import NotFound from "./pages/NotFound.jsx";
+import ResetPassword from './pages/ResetPassword';
+import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
+
 
 // import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <img src={icon} className="App-icon" alt="lady icon" />
-        <p>
-          READY TO GET FIT?
-        </p>
-        <p>
-          Get started below!
-        </p>
-        <a className="App-link"
-          href="https://adaptedstrength.com"
-          target="_blank"
-          rel="noopener noreferrer">
-          Link to beta app
-        </a>
-      </header>
-      <nav class = "App-nav">
-        <NavBar></NavBar>
-      </nav>
-      <BrowserRouter>
-        <Routes>
+    <div className="App h-full my-0">
+      <BrowserRouter className="">
+        <Routes className="">
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path="forgot-password" element={<ForgotPassword />} />
@@ -46,6 +29,9 @@ function App() {
             {/*Example:  <Route path="sign-up" element={<SignUp/>} /> */}
 
             {/* When no route available we go to not found */}
+            {/*Example:  <Route path="sign-up" element={<SignUp/>} /> */}
+            <Route path="reset-password" element={<ResetPassword />} />
+            <Route path="login" element={<Login />} />
             <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
---
+++ frontend/adapted_strength-app/src/pages/Login.jsx
@@ -0,0 +1,5 @@
+export default function Login() {
+    return (<div>
+        <h1>Login</h1>
+    </div>)
+};
