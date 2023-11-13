b9c0c1d54ae377cbff82d26cdadddac421f33a95
Sanguine-Choleric <130211779+Sanguine-Choleric@users.noreply.github.com>
2023-11-13 13:15:45 -0800

    [ADAPTEDS-51] Added password check - TEMP, def redo this later

--- authorization/src/main/java/com/terabite/authorization/controller/AuthorizationController.java
+++ authorization/src/main/java/com/terabite/authorization/controller/AuthorizationController.java
@@ -9,8 +9,11 @@ import com.terabite.authorization.repository.UserRepository;
 import com.terabite.authorization.model.Login;
 import com.terabite.authorization.model.UserInformation;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/v1/user")
@@ -25,19 +28,43 @@ public class AuthorizationController {
 
     @PostMapping("/signup")
     @ResponseStatus(HttpStatus.CREATED)
-    public UserInformation userSignupPost(@RequestBody UserInformation userInformation) throws JsonProcessingException
-    {
+    public UserInformation userSignupPost(@RequestBody UserInformation userInformation) throws JsonProcessingException {
         memberRepository.save(userInformation);
         return userInformation;
     }
 
     @PostMapping("/login")
     public ResponseEntity<?> userLoginPost(@RequestBody Login login) {
-        login.setLoginStatus(LoginStatus.LOGGED_IN);
-        loginRepository.save(login);
+        // Will move to service class
+        // Make sure login exists
+        Login storedLogin = loginRepository.findByEmail(login.getEmail())
+                .orElseThrow(() -> new LoginNotFoundException(login.getEmail()));
 
-        return ResponseEntity.ok(loginRepository.findByEmail(login.getEmail())
-                        .orElseThrow(() -> new LoginNotFoundException(login.getEmail())));
+
+        // Excuse this dogshit
+        // Conditionals check cases of good/bad passwords and existing login/logout status
+        if (Objects.equals(storedLogin.getPassword(), login.getPassword()) && storedLogin.getLoginStatus() == LoginStatus.LOGGED_OUT) {
+            // Good password and not yet logged in
+            login.setLoginStatus(LoginStatus.LOGGED_IN);
+            loginRepository.save(login);
+
+            return ResponseEntity.ok(login);
+
+        } else if (storedLogin.getLoginStatus() == LoginStatus.LOGGED_IN){
+            // Weird case for attempted log in, but user is already logged in
+            login.setLoginStatus(LoginStatus.LOGGED_IN);
+
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(login);
+        } else {
+            // Bad password, not logged in
+            login.setLoginStatus(LoginStatus.LOGGED_OUT);
+
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(login);
+        }
     }
 
     @PostMapping("/logout")
