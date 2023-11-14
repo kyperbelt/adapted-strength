402588e74f46a9f1a268fa91aa3f8357c7ca883c
CaseyCubed <casey.paras@yahoo.com>
2023-11-14 11:41:14 -0800

    Fixing branch

---
+++ authorization/src/main/java/com/terabite/authorization/service/Address.java
@@ -0,0 +1,79 @@
+package com.terabite.authorization.service;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+@Entity
+@Table(name = "address_table")
+public class Address
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @OneToOne(mappedBy = "address")
+    private UserInformation userInformation;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private String city;
+
+    @NotNull
+    private String state;
+
+    @NotNull
+    @Pattern(regexp = "^[0-9]{5}$")
+    private String zipcode;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public UserInformation getUserInformation() {
+        return userInformation;
+    }
+
+    public void setUserInformation(UserInformation userInformation) {
+        this.userInformation = userInformation;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+}
