fe116945282aa4126f905de53d5fae43546ef2d9
Jonathan Camarena <jonathan@DESKTOP-UUH7EJJ>
2023-11-18 22:42:56 -0800

    launcher command parsing

--- tools/tera-launch/src/main.rs
+++ tools/tera-launch/src/main.rs
@@ -33,7 +33,7 @@ enum Commands {
 
 // read a toml file
 // parse it into a struct
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Clone, Deserialize)]
 struct TLaunch {
     global_variables: Option<HashMap<String, Variable>>,
     targets: HashMap<String, Target>,
@@ -46,7 +46,7 @@ type Variable = String;
 // }
 
 // targets can have dependencies on other targets
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Clone, Deserialize)]
 struct Target {
     variables: Option<HashMap<String, String>>,
     description: Option<String>,
@@ -54,7 +54,7 @@ struct Target {
     dependencies: Option<Vec<String>>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Clone, Deserialize)]
 struct Step {
     command: String,
     args: Option<Vec<String>>,
@@ -63,7 +63,43 @@ struct Step {
     piped: Option<bool>,
 }
 
-fn run_target(target: Option<String>, file: String) -> bool {
+fn run_target(target_name: String, file: String) -> bool {
+    let tlaunch = read_tlaunch_file(file);
+    let target = tlaunch.targets.get(&target_name);
+    if target.is_none() {
+        println!("Target {} not found", target_name);
+        return false;
+    }
+    let target = target.unwrap();
+    print!("Running target {}", target_name);
+    for (i, step) in target.clone().steps.iter().enumerate() {
+        print!("\n\tStep [{}]: {}", i, step.command);
+        if let Some(args) = step.args.clone() {
+            for arg in args {
+                print!(" {}", arg);
+            }
+        }
+        if let Some(output) = step.output.clone() {
+            print!(" > {}", output);
+        }
+        if let Some(piped) = step.piped.clone() {
+            print!(" | {}", piped);
+        }
+        // run the command
+        // if it fails, return false
+        // if it succeeds, continue
+
+        let mut command = std::process::Command::new(step.command.clone());
+        command.args(step.args.clone().unwrap_or(vec![]));
+        let output = command.output().unwrap();
+        if !output.status.success() {
+            println!("Step[{}] Command failed: {}", i, step.command);
+            return false;
+        } else if let Some(output_file) = step.output.clone() {
+            std::fs::write(output_file, output.stdout).unwrap();
+        }
+    }
+
     true
 }
 
@@ -74,14 +110,19 @@ fn main() {
     let default_file = "TLAUNCH.toml".to_string();
     println!("{:?}", cli);
     if let Commands::Run { target, file } = cli.cmd {
-        run_target(target, file.unwrap_or(default_file));
+        run_target(
+            target.unwrap_or("all".to_string()),
+            file.unwrap_or(default_file),
+        );
     } else if let Commands::List { file } = cli.cmd {
         list_targets(file.unwrap_or(default_file));
     }
+}
 
-    let tlaunch = read_tlaunch_file();
-
-    for (target_name, target) in tlaunch.targets {
+fn read_tlaunch_file(file: String) -> TLaunch {
+    let toml_str = std::fs::read_to_string(file).unwrap();
+    let tlaunch: TLaunch = toml::from_str(&toml_str).unwrap();
+    for (target_name, target) in tlaunch.clone().targets {
         println!("Target: {}", target_name);
 
         println!("\tDescription: ");
@@ -99,10 +140,5 @@ fn main() {
             }
         }
     }
-}
-
-fn read_tlaunch_file() -> TLaunch {
-    let toml_str = std::fs::read_to_string("TLAUNCH.toml").unwrap();
-    let tlaunch: TLaunch = toml::from_str(&toml_str).unwrap();
     tlaunch
 }
