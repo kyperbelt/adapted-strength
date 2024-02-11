5b955094f070bed5c5ad0d8c267ae64a3c017338
David Castrejon <david.j.castrejon@gmail.com>
2024-02-10 17:20:45 -0800

    Removed login check within subscriptionService

--- authorization/src/main/java/com/terabite/authorization/service/SubscriptionService.java
+++ authorization/src/main/java/com/terabite/authorization/service/SubscriptionService.java
@@ -28,7 +28,7 @@ public class SubscriptionService {
             log.info("Found email: {}", request.username());
             UserInformation existingUser = userRepository.findByLogin_Email(request.username()).orElse(null);
 
-            if (existingUser != null && existingUser.getLogin() != null && existingUser.getLogin().getLoginStatus() == LoginStatus.LOGGED_IN) {
+            if (existingUser != null) {
                 existingUser.setSubscriptionTier(request.subscriptionTier());
 
                 // Save the updated user
@@ -36,16 +36,13 @@ public class SubscriptionService {
 
                 // Fetch the updated user from the database
                 existingUser = userRepository.findByLogin_Email(request.username()).orElse(null);
+                log.info("Updated User Information: email={}, subscriptionTier={}", existingUser.getLogin().getEmail(), existingUser.getSubscriptionTier());
+                return ResponseEntity.ok("Subscription tier updated successfully.");
 
-                if (existingUser != null) {
-                    log.info("Updated User Information: email={}, subscriptionTier={}", existingUser.getLogin().getEmail(), existingUser.getSubscriptionTier());
-                    return ResponseEntity.ok("Subscription tier updated successfully.");
-                } else {
-                    return ResponseEntity.badRequest().body("Failed to retrieve updated user information.");
-                }
             } else {
-                return ResponseEntity.badRequest().body("User is not logged in.");
+                return ResponseEntity.badRequest().body("Failed to retrieve updated user information.");
             }
+
         } else {
             return ResponseEntity.badRequest().body("User not found with the provided email.");
         }
