50742fc8d3b2e460187e3ae6259464ec48d600c0
CaseyCubed <casey.paras@yahoo.com>
2024-03-05 11:53:31 -0800

    Rough draft complete on hamburger menu. Some layering issues, but functionality is working

--- frontend/adapted_strength-app/src/components/navBar.jsx
+++ frontend/adapted_strength-app/src/components/navBar.jsx
@@ -33,19 +33,20 @@ export default function NavBar() {
       <div className={`fixed top-0 right-0 bottom-0 left-0 bg-black opacity-50 z-100 ${hamburgerOpen ? 'block' : 'hidden'}`} onClick={toggleHammy}></div>
       <nav className={`fixed top-0 right-0 h-full bg-gray-900 text-white w-64 py-8 px-4 transform transition-transform ease-in-out duration-300 ${hamburgerOpen ? 'translate-x-0' : 'translate-x-full'}`}>
         <div className="flex flex-col">
-          <Link to="/" className="py-2 hover:bg-gray-800" onClick={toggleHammy}>Home</Link>
-          <Link to="/about" className="py-2 hover:bg-gray-800" onClick={toggleHammy}>About Us</Link>
-          <Link to="/forgot-password" className="py-2 hover:bg-gray-800" onClick={toggleHammy}>Forgot Password</Link>
-          <Link to="/program-management" className="py-2 hover:bg-gray-800" onClick={toggleHammy}>Manage Programs</Link>
-          <Link to="/profile" className="py-2 hover:bg-gray-800" onClick={toggleHammy}>Profile</Link>
-          <Link to="/consultations" className="py-2 hover:bg-gray-800" onClick={toggleHammy}>Calendly Booking</Link>
-          <Link to="/sign-up" className="py-2 hover:bg-gray-800" onClick={toggleHammy}>Sign Up</Link>
+          <Link to="/" className="py-2 my-1 bg-gray-700 rounded-full hover:bg-gray-800" onClick={toggleHammy}>HOME</Link>
+          <Link to="/consultations" className="py-2 my-1 bg-gray-700 rounded-full hover:bg-gray-800" onClick={toggleHammy}>Book a Coaching Session</Link>
+          <Link to="/login" className="py-2 my-1 bg-gray-700 rounded-full hover:bg-gray-800" onClick={toggleHammy}>Login</Link>
+          <Link to="/about" className="py-2 my-1 bg-gray-700 rounded-full hover:bg-gray-800" onClick={toggleHammy}>About Us</Link>
+          <Link to="/forgot-password" className="py-2  my-1 bg-gray-700 rounded-full hover:bg-gray-800" onClick={toggleHammy}>Forgot Password</Link>
+          <Link to="/program-management" className="py-2  my-1 bg-gray-700 rounded-full hover:bg-gray-800" onClick={toggleHammy}>Manage Programs</Link>
+          <Link to="/profile" className="py-2 my-1 bg-gray-700 rounded-full hover:bg-gray-800" onClick={toggleHammy}>Profile</Link>
+          <Link to="/sign-up" className="py-2 my-1 bg-gray-700 rounded-full hover:bg-gray-800" onClick={toggleHammy}>Sign Up</Link>
         </div>
       </nav>
-      <div className="fixed top-8 right-8 z-50 cursor-pointer" onClick={toggleHammy}>
-        <div className={`w-6 h-0.5 bg-white mb-1 rounded ${hamburgerOpen ? 'transform rotate-45 translate-y-1.5' : ''}`}></div>
-        <div className={`w-6 h-0.5 bg-white mb-1 rounded ${hamburgerOpen ? 'opacity-0' : ''}`}></div>
-        <div className={`w-6 h-0.5 bg-white mb-1 rounded ${hamburgerOpen ? 'transform -rotate-45 -translate-y-1.5' : ''}`}></div>
+      <div className="h-9 w-9 bg-black border-solid border-2 border-red-900 rounded-full content-center fixed top-3 right-3 z-50 cursor-pointer" onClick={toggleHammy}>
+        <div className={`w-6 h-0.5 bg-white mt-2 ml-1 mb-1 rounded ${hamburgerOpen ? 'transform rotate-45 translate-y-1.5' : ''}`}></div>
+        <div className={`w-6 h-0.5 bg-white mt-1 ml-1 mb-1 rounded ${hamburgerOpen ? 'opacity-0' : ''}`}></div>
+        <div className={`w-6 h-0.5 bg-white mt-1 ml-1 mb-1 rounded ${hamburgerOpen ? 'transform -rotate-45 -translate-y-1.5' : ''}`}></div>
       </div>
     </div>
   );
