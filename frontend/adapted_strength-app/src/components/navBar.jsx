5199a3c1bf53ebb1ad9c11092b28310e768f3d24
CaseyCubed <casey.paras@yahoo.com>
2024-03-05 11:57:03 -0800

    Rough draft complete on hamburger menu. Fixed layering with z-0 fix

--- frontend/adapted_strength-app/src/components/navBar.jsx
+++ frontend/adapted_strength-app/src/components/navBar.jsx
@@ -29,7 +29,7 @@ export default function NavBar() {
   //      certain options when the user is not logged in for example, or if they 
   //      dont have the right permissions or are in a wrong state/certain page/step.
   return (
-    <div>
+    <div className={'z-50'}>
       <div className={`fixed top-0 right-0 bottom-0 left-0 bg-black opacity-50 z-100 ${hamburgerOpen ? 'block' : 'hidden'}`} onClick={toggleHammy}></div>
       <nav className={`fixed top-0 right-0 h-full bg-gray-900 text-white w-64 py-8 px-4 transform transition-transform ease-in-out duration-300 ${hamburgerOpen ? 'translate-x-0' : 'translate-x-full'}`}>
         <div className="flex flex-col">
