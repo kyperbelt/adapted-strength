9f4a34e785d00a86e5dbca63cbaa1fa79b6a6243
unknown <zdannythebest@gmail.com>
2024-03-05 20:52:26 -0800

    Added functionality for sending dynamic text

--- frontend/adapted_strength-app/src/pages/Chat.jsx
+++ frontend/adapted_strength-app/src/pages/Chat.jsx
@@ -1,3 +1,5 @@
+import {useState} from "react";
+
 function RightMessage({...props})
 {
     return (
@@ -25,8 +27,8 @@ function Subscribed()
     return (
         <>
             {/* Need to make sure button is circular and allows file to be uploaded */}
-            <input type="file" id="file-input" class="hidden"/>
-            <label for="file-input" className="rounded-full bg-custom-red">+</label>
+            {/*<input type="file" id="file-input" class="hidden"/>*/}
+            {/*<label for="file-input" className="rounded-full bg-custom-red">+</label>*/}
             <input type="text" placeholder="Type your message..." className="flex-1 border rounded-full px-4 py-2 focus:outline-none"/>
             <button className="rounded-full bg-custom-red block p-2 ml-2 hover:bg-custom-dark-red">Send</button>
         </>
@@ -55,13 +57,31 @@ function ChatBox({...props})
 }
 
 export default function Chat() {
+    const [msg, setMsg] = useState("");
+    const [list, setList] = useState([]);
+
+    const click = () => {
+        alert(msg)
+    }
+
+    const change = event => {
+        setMsg(event.target.value)
+    }
+
+    const addToList = () => {
+        let tempList = list;
+        tempList.push(msg);
+        setList(tempList);
+        setMsg("");
+    }
+
     return (
         <div className="bg-white m-2 rounded-lg p-2">
             <div className="font-semibold bg-custom-gray p-2">
                 Chatting with Alex
             </div>
             <div className="flex-1 overflow-y-auto p-4">
-                <div className="flex flex-col space-y-2">
+                <div className="flex flex-col space-y-2" id="a">
                     <RightMessage>
                         Sender: msg 1
                     </RightMessage>
@@ -86,10 +106,13 @@ export default function Chat() {
                     <LeftMessage>
                         Receiver: msg 8
                     </LeftMessage>
+                    <ul>{list.length > 0 && list.map((item) => <RightMessage>{item}</RightMessage>)}</ul>
                 </div>
             </div>
             <div className="bg-custom-gray p-4 flex items-center">
-                <ChatBox isSubscribed={true}/>
+                {/*<ChatBox isSubscribed={true}/>*/}
+                <input type="text" placeholder="Type your message..." className="flex-1 border rounded-full px-4 py-2 focus:outline-none" value={msg} onChange={change}/>
+                <button className="rounded-full bg-custom-red block p-2 ml-2 hover:bg-custom-dark-red" onClick={addToList}>Send</button>
             </div>
         </div>
     );
