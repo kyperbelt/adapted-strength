3bf55d9c397e6e9cc3d8c654cc609432e5460d25
CaseyCubed <casey.paras@yahoo.com>
2024-03-10 16:21:40 -0700

    Added template to all programming pages

--- frontend/adapted_strength-app/src/pages/program_pages/Power.jsx
+++ frontend/adapted_strength-app/src/pages/program_pages/Power.jsx
@@ -4,6 +4,7 @@ Team: TeraBITE
 */
 //import { Outlet, Link } from "react-router-dom";
 import programFiller from "../../assets/blank_filler.png"
+import { CardBack } from "../../components/Card";
 
 /*
 function ClientField() {
@@ -17,9 +18,64 @@ export default function Power() {
     return (
         <div className="w-full h-full flex flex-col bottom-20">
             <p className="bg-[#161A1D] text-white bottom-3 px-0 pt-8 pb-8">
-            YOUR PROGRAM: Powerlifting!
+                YOUR PROGRAM: Powerlifting!
             </p>
-            <img src={programFiller} className="h-20 w-20 grid place-content-center flex flex-col boarder-2" alt="workout program" />
+            <CardBack className={`xl:w-11/12 xl:mx-auto w-full`}>
+                <div className="overflow-x-auto">
+                    <h1 className="font-bold uppercase bg-gray-100">
+                        March 1 - April 5
+                    </h1>
+                    <table className="w-full text-sm text-center rtl:text-right ">
+                        <tbody className="rounded-full text-[#161A1D]">
+                            <tr>
+                                {/* LET "Order" AND "Exercise" CHANGE! 
+                                This is hard coded for UI purposes */}
+                                <th scope="col" className="px-1.5 py-1 bg-gray-300">
+                                    Order
+                                </th>
+                                <th scope="col" className="px-1.5 py-1 border-solid border-2 border-black bg-gray-300">
+                                    Exercise
+                                    <td scope="col" className="px-1.5 text-xs bg-gray-200">
+                                        Equipment
+                                    </td>
+                                    <td scope="col" className="px-1.5 text-xs bg-gray-100">
+                                        Sets
+                                    </td>
+                                    <td scope="col" className="px-1.5 text-xs bg-gray-200">
+                                        Reps/Time
+                                    </td>
+                                    <td scope="col" className="px-1.5 text-xs bg-gray-100">
+                                        % or RPE
+                                    </td>
+                                    <td scope="col" className="px-1.5 text-xs bg-gray-200">
+                                        Rest
+                                    </td>
+                                    <tbody className="text-s rounded-full text-[#161A1D] bg-gray-100">
+                                        {/* LET ALL entries below change.
+                                        This is hard coded for UI purposes */}
+                                        <td scope="row" className="px-1.5 bg-gray-200">
+                                            BB
+                                        </td>
+                                        <td scope="row" className="px-1.5 bg-gray-100">
+                                            3-4
+                                        </td>
+                                        <td scope="row" className="px-1.5 bg-gray-200">
+                                            10-12
+                                        </td>
+                                        <td scope="row" className="px-1.5 bg-gray-100">
+                                            60
+                                        </td>
+                                        <td scope="row" className="px-1.5 bg-gray-200">
+                                            2-3 min
+                                        </td>
+                                    </tbody>
+                                </th>
+                            </tr>
+                        </tbody>
+
+                    </table>
+                </div>
+            </CardBack>
         </div>
     );
 }
