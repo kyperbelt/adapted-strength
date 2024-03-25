83054c6aaedae7d4e1debace37c360d4e983be55
Joshua Cupler <joshuacupler@gmail.com>
2024-03-25 16:57:12 -0700

    init

--- authorization/src/main/java/com/terabite/video/controller/VideoController.java
+++ authorization/src/main/java/com/terabite/video/controller/VideoController.java
@@ -1,45 +1,104 @@
-// package com.terabite.video.controller;
-//
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.DeleteMapping;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RequestParam;
-// import org.springframework.web.bind.annotation.RestController;
-// import org.springframework.web.multipart.MultipartFile;
-//
-// import com.terabite.video.service.VideoService;
-//
-// import io.awspring.cloud.s3.S3Resource;
-// import jakarta.servlet.http.HttpServletRequest;
-//
-// @RestController
-// @RequestMapping("/v1/video")
-// public class VideoController {
-//     private final VideoService videoService;
-//
-//     public VideoController(VideoService videoService){
-//         this.videoService = videoService;
-//     }
-//
-//     @PostMapping("")
-//     public ResponseEntity<?> uploadVideo(@RequestParam("video") MultipartFile video, @RequestParam("video_name") String videoName, @RequestParam("client") String client, HttpServletRequest request){
-//         return videoService.uploadVideo(video, videoName, client);
-//     }
-//
-//     @GetMapping("")
-//     public ResponseEntity<S3Resource> getVideo(@RequestParam("video_name") String videoName, @RequestParam("client") String client, HttpServletRequest request){
-//         return videoService.downloadVideo(videoName, client);
-//     }
-//
-//     @DeleteMapping("")
-//     public ResponseEntity<?> deleteVideo(@RequestParam("video_name") String videoName, @RequestParam("client") String client, HttpServletRequest request){
-//         return videoService.deleteVideo(videoName, client);
-//     }
-//
-//     @DeleteMapping("/all")
-//     public ResponseEntity<?> deleteAllClientVideo(@RequestParam("client") String client, HttpServletRequest request){
-//         return videoService.deleteBucket(client);
-//     }
-// }
+package com.terabite.video.controller;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.terabite.video.service.VideoService;
+
+import io.awspring.cloud.s3.S3Resource;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Optional;
+import java.util.List;
+
+// Given that we're using email as the login / username, it should (hopefully) be fine to just use the username form the auth
+
+@RestController
+@RequestMapping("/v1/video")
+public class VideoController {
+    private final VideoService videoService;
+
+    public VideoController(VideoService videoService){
+        this.videoService = videoService;
+    }
+
+    // Coach endpoints
+    @PostMapping("/coach")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public ResponseEntity<?> uploadVideo(@AuthenticationPrincipal UserDetails userDetails, @RequestParam("video") MultipartFile video, @RequestParam("video_name") String videoName){
+        return videoService.uploadCoachVideo(video, videoName);
+        // request.getCookies()
+        // )).anyMatch(str -> str.equals("ROLE_ADMIN") );
+    }
+
+    @GetMapping("/coach/{video}")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public ResponseEntity<S3Resource> getVideo(@AuthenticationPrincipal UserDetails userDetails, @RequestParam("video") String videoName, @RequestParam("client") Optional<String> client){
+        if(client.isEmpty() ) return videoService.downloadCoachVideo(videoName);
+        else return videoService.downloadClientVideo(videoName, client.get() );
+    }
+
+    // @GetMapping("")
+    // @PreAuthorize("!hasAuthority('ROLE_UNVERIFIED')")
+    // public ResponseEntity<List<String>> getVideoNameList(@AuthenticationPrincipal UserDetails userDetails, @RequestParam("video") String videoName, @RequestParam("client") Optional<String> client){
+    //     if(client.isEmpty() ) return videoService.downloadCoachVideo(videoName);
+    //     if(isAdmin(userDetails) ) return videoService.downloadClientVideo(videoName, client.get() );
+    //     return videoService.downloadClientVideo(videoName, userDetails.getUsername() );
+    // }
+
+
+    @DeleteMapping("/coach/{video}")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public ResponseEntity<?> deleteVideo(@AuthenticationPrincipal UserDetails userDetails, @RequestParam("video_name") String videoName, @RequestParam("client") Optional<String> client){
+        if(client.isEmpty() ) return videoService.deleteCoachVideo(videoName);
+        else return videoService.deleteClientVideo(videoName, client.get() );
+    }
+
+    // Client endpoints
+
+    @PostMapping("/client")
+    @PreAuthorize("!hasAuthority('ROLE_UNVERIFIED')")
+    public ResponseEntity<?> uploadClientVideo(@AuthenticationPrincipal UserDetails userDetails, @RequestParam("video") MultipartFile video, @RequestParam("video_name") String videoName){
+        return videoService.uploadClientVideo(video, videoName, userDetails.getUsername() );
+        // request.getCookies()
+        // )).anyMatch(str -> str.equals("ROLE_ADMIN") );
+    }
+
+    @GetMapping("/client/{video}")
+    @PreAuthorize("!hasAuthority('ROLE_UNVERIFIED')")
+    public ResponseEntity<S3Resource> getVideo(@AuthenticationPrincipal UserDetails userDetails, @RequestParam("video") String videoName) {
+        if(client.isEmpty() ) return videoService.downloadCoachVideo(videoName);
+        else return videoService.downloadClientVideo(videoName, client.get() );
+    }
+
+    @DeleteMapping("/{video}")
+    @PreAuthorize("!hasAuthority('ROLE_UNVERIFIED')")
+    public ResponseEntity<?> deleteVideo(@AuthenticationPrincipal UserDetails userDetails, @RequestParam("video_name") String videoName) {
+        if(client.isEmpty() ) return videoService.deleteCoachVideo(videoName);
+        else return videoService.deleteClientVideo(videoName, client.get() );
+    }
+
+
+    // @DeleteMapping("/all")
+    // @PreAuthorize("!hasAuthority('ROLE_UNVERIFIED')")
+    // public ResponseEntity<?> deleteAllClientVideo(@AuthenticationPrincipal UserDetails userDetails, @RequestParam("client") String client, HttpServletRequest request){
+    //     return videoService.deleteBucket(client);
+    // }
+
+    // private boolean isAdmin(UserDetails details){
+    //     return details.getAuthorities().stream().map(GrantedAuthority::getAuthority).anyMatch("ROLE_ADMIN"::equals);
+    // }    
+    
+    // @PreAuthorize("!hasAuthority('ROLE_UNVERIFIED')")
+
+}
--- authorization/src/main/java/com/terabite/video/service/VideoService.java
+++ authorization/src/main/java/com/terabite/video/service/VideoService.java
@@ -1,117 +1,170 @@
-// package com.terabite.video.service;
-//
-// import java.io.IOError;
-// import java.io.IOException;
-// import java.io.InputStream;
-// import java.nio.file.Files;
-// import java.util.Optional;
-//
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.multipart.MultipartFile;
-//
-// import io.awspring.cloud.s3.S3Resource;
-// import io.awspring.cloud.s3.S3Template;
-//
-// import org.springframework.core.io.Resource;
-//
-// /**
-//  * A service that processes video files.
-//  * Coupled with the AWS S3 service for persistent storage of video files
-//  * As such, each video is stored in an client-specific bucket, and each video has an video name unique to said bucket
-//  * Ensure that the env vars AWS_SECRET_ACCESS_KEY && AWS_ACCESS_KEY_ID are set
-//  */
-// @Configuration
-// public class VideoService {
-//     private S3Template s3Template;
-//
-//     public VideoService(S3Template s3Template){
-//         this.s3Template = s3Template;
-//     }
-//     
-//     public ResponseEntity<?> uploadVideo(MultipartFile file, String videoName, String clientBucket){
-//         if(!checkFileType(file.getResource(), file.getContentType() ) ) return new ResponseEntity<>(HttpStatus.UNSUPPORTED_MEDIA_TYPE);
-//         ensureClientBucketExists(clientBucket);
-//         try(InputStream stream = file.getInputStream() ) {
-//             uploadFile(clientBucket, videoName, stream);            
-//             return new ResponseEntity<>(HttpStatus.ACCEPTED); 
-//         }
-//         catch(IOException e){
-//             return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
-//         }
-//     }
-//
-//     public ResponseEntity<S3Resource> downloadVideo(String videoName, String clientBucket){
-//         if(!checkIfClientBucketExist(clientBucket) ) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-//         if(!checkIfFileExists(clientBucket, videoName) ) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//         return new ResponseEntity<>(downloadFile(clientBucket, videoName), HttpStatus.OK);
-//     }
-//
-//     public ResponseEntity<?> deleteVideo(String videoName, String clientBucket) {
-//         if(!checkIfClientBucketExist(clientBucket) ) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-//         if(!checkIfFileExists(clientBucket, videoName) ) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//         deleteFile(clientBucket, videoName);
-//         return new ResponseEntity<>(HttpStatus.OK);
-//
-//     }   
-//
-//     public ResponseEntity<?> deleteBucket(String clientBucket){
-//         if(checkIfClientBucketExist(clientBucket)) {
-//             s3Template.deleteBucket(clientBucket);
-//             return new ResponseEntity<>(HttpStatus.OK);
-//         }
-//         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//     }
-//     
-//     // ***********************************************************
-//     
-//     /**
-//      * Checks if the given file is in an acceptable video format
-//      * @param resource the resource to check
-//      * @param fileType the MIME type, if known
-//      * @return true if acceptable, false otherwise
-//      */
-//     private boolean checkFileType(Resource resource, Optional<String> fileType){
-//         String content = fileType.orElseGet(() -> "unknown");
-//         return content.contains("video");
-//
-//         // TODO check resource for file type by interrogating file contents, as opposed to just using the MIME
-//         // This is for security reasons, as the MIME type is self-reported, and as such, can be faked
-//     }
-//
-//     /**
-//      * Overloaded Method
-//      * @param resource
-//      * @param fileType
-//      * @return
-//      */
-//     private boolean checkFileType(Resource resource, String fileType){
-//         return checkFileType(resource, Optional.ofNullable(fileType) );
-//     }
-//
-//     private boolean checkIfClientBucketExist(String bucket){
-//         return s3Template.bucketExists(bucket);
-//     }
-//
-//     private void ensureClientBucketExists(String bucket){
-//         if(checkIfClientBucketExist(bucket) ) return;
-//         else s3Template.createBucket(bucket);
-//     }
-//
-//     private boolean checkIfFileExists(String bucket, String fileName){
-//         return s3Template.objectExists(bucket, fileName);
-//     }
-//
-//     private void uploadFile(String bucket, String fileName, InputStream file){
-//         s3Template.upload(bucket, fileName, file);
-//     }
-//
-//     private S3Resource downloadFile(String bucket, String fileName) {
-//         return s3Template.download(bucket, fileName);
-//     }
-//
-//     private void deleteFile(String bucket, String file){
-//         s3Template.deleteObject(bucket, file);
-//     }
-// }
+package com.terabite.video.service;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Optional;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
+
+import io.awspring.cloud.s3.S3Resource;
+import io.awspring.cloud.s3.S3Template;
+
+import org.springframework.core.io.Resource;
+import java.util.List;
+
+/**
+ * A service that processes video files.
+ * Coupled with the AWS S3 service for persistent storage of video files
+ * As such, each video is stored in an client-specific bucket, and each video has an video name unique to said bucket
+ * Ensure that the env vars AWS_SECRET_ACCESS_KEY && AWS_ACCESS_KEY_ID are set
+ */
+@Configuration
+public class VideoService {
+    private S3Template s3Template;
+    private static final String CLIENT_BUCKET = "client-video";
+    private static final String COACH_BUCKET = "coach-video";
+
+    public VideoService(S3Template s3Template){
+        this.s3Template = s3Template;
+        ensureBucketExists(CLIENT_BUCKET);
+        ensureBucketExists(COACH_BUCKET);
+    }
+
+    // public List<String> getClientVideoList(){
+    //     s3Template.listObjects(COACH_BUCKET, CLIENT_BUCKET);
+    // }
+
+    // public List<String> getCoachVideoList(){
+    //     s3Template.
+    // }
+    
+    public ResponseEntity<?> uploadClientVideo(MultipartFile file, String videoName, String client){
+        return uploadVideo(file, CLIENT_BUCKET, videoName, client);
+    }
+
+    public ResponseEntity<?> uploadCoachVideo(MultipartFile file, String videoName){
+        return uploadVideo(file, COACH_BUCKET, videoName);
+    }
+
+    public ResponseEntity<S3Resource> downloadClientVideo(String videoName, String client){
+        Optional<S3Resource> video = downloadVideo(CLIENT_BUCKET, videoName, client);
+        if(video.isEmpty() ) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(video.get(), HttpStatus.OK);
+    }
+
+    public ResponseEntity<S3Resource> downloadCoachVideo(String videoName){
+        Optional<S3Resource> video = downloadVideo(COACH_BUCKET, videoName);
+        if(video.isEmpty() ) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(video.get(), HttpStatus.OK);
+    }
+
+    public ResponseEntity<?> deleteClientVideo(String videoName, String client) {
+        if(!checkIfFileExists(CLIENT_BUCKET, videoName, client) ) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        if(deleteVideo(CLIENT_BUCKET, videoName, client) ) return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
+    }   
+
+    public ResponseEntity<?> deleteCoachVideo(String videoName) {
+        if(!checkIfFileExists(COACH_BUCKET, videoName) ) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        if(deleteVideo(COACH_BUCKET, videoName) ) return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
+    }   
+    
+    // ***********************************************************
+    private ResponseEntity<?> uploadVideo(MultipartFile file, String bucket, String videoName){
+        // return uploadVideo(file, videoName, "");
+        if(!checkFileType(file.getResource(), file.getContentType() ) ) return new ResponseEntity<>(HttpStatus.UNSUPPORTED_MEDIA_TYPE);
+        if(checkIfFileExists(bucket, videoName) ) return new ResponseEntity<>(HttpStatus.CONFLICT);
+        try(InputStream stream = file.getInputStream() ){
+            if(uploadFile(bucket, videoName, stream) ) return new ResponseEntity<>(HttpStatus.ACCEPTED);
+        }
+        catch(IOException e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
+    }
+    
+    private ResponseEntity<?> uploadVideo(MultipartFile file, String bucket, String videoName, String prefix){
+        return uploadVideo(file, bucket, prefix + videoName);
+        // if(!checkFileType(file.getResource(), file.getContentType() ) ) return new ResponseEntity<>(HttpStatus.UNSUPPORTED_MEDIA_TYPE);
+        // // ensureClientBucketExists(clientBucket);
+        // try(InputStream stream = file.getInputStream() ) {
+        //     uploadFile(clientBucket, videoName, stream);            
+        //     return new ResponseEntity<>(HttpStatus.ACCEPTED); 
+        // }
+        // catch(IOException e){
+        //     return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
+        // }
+    }
+
+    private Optional<S3Resource> downloadVideo(String bucket, String videoName){
+        if(!checkIfFileExists(bucket, videoName) ) return Optional.empty();
+        return Optional.of(downloadFile(bucket, videoName) );
+    }
+
+    private Optional<S3Resource> downloadVideo(String bucket, String videoName, String prefix){
+        return downloadVideo(bucket, prefix + videoName);
+    }
+
+    private boolean deleteVideo(String videoName, String clientBucket) {
+        deleteFile(clientBucket, videoName);
+        return checkIfFileExists(clientBucket, clientBucket);
+
+    }   
+    private boolean deleteVideo(String videoName, String clientBucket, String prefix) {
+        return deleteVideo(videoName, prefix + clientBucket);
+    }
+
+    /**
+     * Checks if the given file is in an acceptable video format
+     * @param resource the resource to check
+     * @param fileType the MIME type, if known
+     * @return true if acceptable, false otherwise
+     */
+    private boolean checkFileType(Resource resource, Optional<String> fileType){
+        String content = fileType.orElseGet(() -> "unknown");
+        return content.contains("video");
+
+        // TODO check resource for file type by interrogating file contents, as opposed to just using the MIME
+        // This is for security reasons, as the MIME type is self-reported, and as such, can be faked
+    }
+
+    /**
+     * Overloaded Method
+     * @param resource
+     * @param fileType
+     * @return
+     */
+    private boolean checkFileType(Resource resource, String fileType){
+        return checkFileType(resource, Optional.ofNullable(fileType) );
+    }
+
+    private void ensureBucketExists(String bucket){
+        if(s3Template.bucketExists(bucket) ) return;
+        else s3Template.createBucket(bucket);
+    }
+
+    private boolean checkIfFileExists(String bucket, String fileName){
+        return s3Template.objectExists(bucket, fileName);
+    }
+
+    private boolean checkIfFileExists(String bucket, String fileName, String prefix){
+        return checkIfFileExists(bucket, prefix + fileName);
+    }
+
+    private boolean uploadFile(String bucket, String fileName, InputStream file){
+        s3Template.upload(bucket, fileName, file);
+        return checkIfFileExists(bucket, fileName);
+    }
+
+    private S3Resource downloadFile(String bucket, String fileName) {
+        return s3Template.download(bucket, fileName);
+    }
+
+    private void deleteFile(String bucket, String file){
+        s3Template.deleteObject(bucket, file);
+    }
+}
