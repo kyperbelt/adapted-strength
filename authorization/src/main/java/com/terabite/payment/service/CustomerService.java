9a01a5573a805fab27e7c097ad50458969064c16
Buh-La-Kay <114424722+Buh-La-Kay@users.noreply.github.com>
2024-03-30 15:27:15 -0700

    Backend stripe services are complete (I think). Still need to finish front end before I can fully test everything.

--- authorization/src/main/java/com/terabite/payment/service/CustomerService.java
+++ authorization/src/main/java/com/terabite/payment/service/CustomerService.java
@@ -1,20 +1,38 @@
 package com.terabite.payment.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
+import com.stripe.model.Subscription;
 import com.terabite.payment.model.Customer;
 import com.terabite.payment.repository.CustomerRepository;
 import com.terabite.user.model.UserInformation;
+import com.terabite.user.repository.UserRepository;
 
 @Service
 public class CustomerService {
     CustomerRepository customerRepository;
+    UserRepository userRepository;
     StripeCustomerService stripeCustomerService;
 
-    public CustomerService(CustomerRepository customerRepository, StripeCustomerService stripeCustomerService){
+    @Value("${ADAPTED_STRENGTH_STRIPE_KEY}")
+    private String stripeKey;
+
+    @Value("${ADAPTED_STRENGTH_BASE_PRICE_ID}")
+    private String baseClientPriceId;
+
+    @Value("${ADAPTED_STRENGTH_GENERAL_PRICE_ID}")
+    private String generalClientPriceId;
+
+    @Value("${ADAPTED_STRENGTH_SPECIFIC_PRICE_ID}")
+    private String specificClientPriceId;
+
+    public CustomerService(CustomerRepository customerRepository, StripeCustomerService stripeCustomerService, UserRepository userRepository){
         this.customerRepository = customerRepository;
         this.stripeCustomerService = stripeCustomerService;
+        this.userRepository = userRepository;
     }
 
 
@@ -24,4 +42,92 @@ public class CustomerService {
         return customer;
     }
 
+
+    // This function returns 1 if the subscriptionStatus == active and 0 if the subscriptionStatus is anything else.
+    // A return value of -1 indicates a resouce was not found
+    public int getSubscriptionByUserId(long userId) {
+        Stripe.apiKey = stripeKey;
+
+        UserInformation userInformation = userRepository.findById(userId).orElse(null);
+
+        if (userInformation!= null){
+            Customer customer = userInformation.getCustomer();
+
+            if(customer!=null){
+                String subscriptionId = customer.getSubscriptionId();
+
+                if(subscriptionId!=null){
+                    try{
+                        Subscription subscription = Subscription.retrieve(subscriptionId);
+                        if (subscription.getStatus() == "active") {
+                            return 1;
+                        }
+                    }
+                    catch (StripeException e){
+                        e.printStackTrace();
+                    }   
+                }
+                else{
+                    return -1;
+                }
+            }
+            else{
+                return -1;
+            }
+        }
+        else{
+            return -1;
+        }
+        return 0;
+    }
+
+
+    /*  
+        This function returns a 0 if the user is unsubscribed, a 1 if the client is a base client,
+        a 2 if the client is a general client, and a 3 if the user is a specific client
+        A negative number indicates a resource was not found 
+    */
+    public int getSubscriptionLevelByUserId(long userId){
+        Stripe.apiKey = stripeKey;
+
+        UserInformation userInformation = userRepository.findById(userId).orElse(null);
+        if(userInformation!= null){
+            Customer customer = userInformation.getCustomer();
+            if(customer!= null){
+                String subscriptionId = customer.getSubscriptionId();
+                if(subscriptionId!=null){
+                    try{
+                        Subscription subscription = Subscription.retrieve(subscriptionId);
+                        String priceId = subscription.getItems().getData().get(6).getPrice().getId();
+                        if(priceId == baseClientPriceId){
+                            return 1;
+                        }
+                        else if (priceId == generalClientPriceId){
+                            return 2;
+                        }
+                        else if (priceId == specificClientPriceId){
+                            return 3;
+                        }
+                        else{
+                            return -1;  //price id does not match one of the three tiers offered. Not sure how this could happen
+                        }
+                        
+                    }
+                    catch(StripeException e){
+                        e.printStackTrace();
+                    }
+                }
+                else{
+                    return 0;  //no subscriptionId means user is not subscribed
+                }
+            }
+            else{ 
+                return -2; //customer was not found
+            }
+        }
+        else{
+            return -3; //userInformation was not found
+        }
+        return -4; //
+    }
 }
--- authorization/src/main/java/com/terabite/payment/service/WebhookService.java
+++ authorization/src/main/java/com/terabite/payment/service/WebhookService.java
@@ -16,6 +16,9 @@ import com.stripe.model.Subscription;
 import com.stripe.net.ApiResource;
 import com.stripe.net.Webhook;
 import com.terabite.payment.repository.CustomerRepository;
+import com.terabite.user.model.SubscriptionStatus;
+import com.terabite.user.model.UserInformation;
+import com.terabite.user.repository.UserRepository;
 
 @Service
 public class WebhookService {
@@ -25,10 +28,22 @@ public class WebhookService {
     @Value("${ADAPTED_STRENGTH_STRIPE_ENDPOINT}")
     private String endpointSecret;
 
+    @Value("${ADAPTED_STRENGTH_BASE_PRICE_ID}")
+    private String baseClientPriceId;
+
+    @Value("${ADAPTED_STRENGTH_GENERAL_PRICE_ID}")
+    private String generalClientPriceId;
+
+    @Value("${ADAPTED_STRENGTH_SPECIFIC_PRICE_ID}")
+    private String specificClientPriceId;
+
     private CustomerRepository customerRepository;
 
-    public WebhookService(CustomerRepository customerRepository){
+    private UserRepository userRepository;
+
+    public WebhookService(CustomerRepository customerRepository, UserRepository userRepository){
         this.customerRepository = customerRepository;
+        this.userRepository = userRepository;
     }
 
     public HttpStatus handleWebhookEvent(String payload, Map<String, String> header){
@@ -61,12 +76,6 @@ public class WebhookService {
             //
         }
 
-        //handle event
-        // if(event.getType() == "payment_intent.succeeded"){
-        //     PaymentIntent paymentIntent = (PaymentIntent) stripeObject;
-        //     handlePaymentIntentSuccess(paymentIntent);
-        // }
-
         if (event.getType() == "customer.subscription.created"){
             Subscription subscription = (Subscription) stripeObject;
             handleCustomerSubscriptionCreated(subscription);
@@ -85,27 +94,82 @@ public class WebhookService {
         return HttpStatus.OK;
     }
 
+
+    // handle the event
+
+    // When a subscription is created all we need to do is associate the subscriptionId with the user in our database
     public void handleCustomerSubscriptionCreated(Subscription subscription){
         String customerId = subscription.getCustomer();
         com.terabite.payment.model.Customer customer;
         if(customerId!= null){
             customer = customerRepository.findById(customerId).orElse(null);
-            customer.setSubscriptionId(subscription.getId());
-            customerRepository.save(customer);
+            if(customer!= null){
+                customer.setSubscriptionId(subscription.getId());
+                customerRepository.save(customer);
+            }
         }
-
     }
 
+    // Subscription deleted means the subscription has ended
     public void handleCustomerSubscriptionDeleted(Subscription subscription){
-
+        UserInformation userInformation = getUserBySubscription(subscription);
+        if (userInformation != null){
+            userInformation.setSubscriptionTier(SubscriptionStatus.NO_SUBSCRIPTION);
+            userRepository.save(userInformation);
+        }
     }
 
     public void handleCustomerSubscriptionUpdated(Subscription subscription){
+        UserInformation userInformation = getUserBySubscription(subscription);
+        
+        //Not sure about this. Should only happen if there is no user associated with the subscription
+        if(userInformation == null){
+            return;
+        }
 
+        /* 
+            Subscription updates can mean many things, so we have to check the status of the subscription before we change anything.
+            First case will be statuses that would result in losing access to content.
+        */
+        if (subscription.getStatus() == "canceled" 
+            || subscription.getStatus() == "paused"                   // paused can only happen when a free trial ends which is not currently a feature but may be later
+            || subscription.getStatus() == "incomplete"               // incomplete happens after a subscription object has been created but before it is paid
+            || subscription.getStatus() == "incomplete_expired"       // incomplete_expired means that the user never finished the payment
+            || subscription.getStatus() == "unpaid"){                 
+                userInformation.setSubscriptionTier(SubscriptionStatus.NO_SUBSCRIPTION);
+        }
+
+
+        // This block will assign the new subscriptionStatus of active subs based on their paymentId
+        else if (subscription.getStatus() == "active"){
+            if(subscription.getItems().getData().get(6).getPrice().getId() == baseClientPriceId){
+                userInformation.setSubscriptionTier(SubscriptionStatus.BASE_CLIENT);
+            }
+            else if(subscription.getItems().getData().get(6).getPrice().getId() == generalClientPriceId){
+                userInformation.setSubscriptionTier(SubscriptionStatus.GENERAL_CLIENT);
+            }
+            else if(subscription.getItems().getData().get(6).getPrice().getId() == specificClientPriceId){
+                userInformation.setSubscriptionTier(SubscriptionStatus.SPECIFIC_CLIENT);
+            }
+        }
     }
 
-    public void handleCustomerSubscriptionResumed(Subscription subscription){
 
+    public UserInformation getUserBySubscription(Subscription subscription){
+        String customerId = subscription.getCustomer();
+        
+        if (customerId == null){
+            return null;
+        }
+        com.terabite.payment.model.Customer customer = customerRepository.findById(customerId).orElse(null);
+
+        if (customer!=null){
+            UserInformation userInformation = userRepository.findByCustomer(customer).orElse(null);
+            return userInformation;
+        }
+        else{
+            return null;
+        }
     }
 
     
