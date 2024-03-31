37a508d396527f6b5501433102207d85b0438c7f
CaseyCubed <casey.paras@yahoo.com>
2024-03-31 14:14:01 -0700

    Functional front end complete for program interface. Backend linking to be completed

--- frontend/adapted_strength-app/src/pages/program_pages/General.jsx
+++ frontend/adapted_strength-app/src/pages/program_pages/General.jsx
@@ -43,60 +43,63 @@ export default function General() {
 
     const getTable = (days) => {
         if (!days.some(day => day)) return null;
-
+      
+        const alphabet = 'ABCDEFGHIJ';
+      
         return (
-            <table className="w-full text-left mt-4">
-                <tbody className="rounded-full text-[#161A1D]">
-                {days.map((day, index) => (
-                    <tr>
-                        {/* LET "Order" AND "Exercise" CHANGE! 
-                                This is hard coded for UI purposes */}
-                        <th scope="col" className="px-1.5 py-1 border-solid border-2 border-black bg-gray-300">
-                            Order
-                        </th>
-                        <th scope="col" className="px-1.5 py-1 border-solid border-2 border-black bg-gray-300">
-                            Movement
-                            <td scope="col" className="px-1.5 text-xs bg-gray-200">
-                                Equipment
-                            </td>
-                            <td scope="col" className="px-3 text-xs bg-gray-100">
-                                Sets
-                            </td>
-                            <td scope="col" className="px-3 text-xs bg-gray-200">
-                                Reps/Time
-                            </td>
-                            <td scope="col" className="px-1.5 text-xs bg-gray-100">
-                                % or RPE
-                            </td>
-                            <td scope="col" className="px-1.5 text-xs bg-gray-200">
-                                Rest
-                            </td>
-                            <tbody className="text-s rounded-full text-[#161A1D] bg-gray-100">
-                                    <tr key={index}>
-                                        <td className="border px-4 py-2">
-                                            {index + 1}
-                                        </td>
-                                        <td className="border px-4 py-2">
-                                            {day.sets}
-                                        </td>
-                                        <td className="border px-4 py-2">
-                                            {day.reps}
-                                        </td>
-                                        <td className="border px-4 py-2">
-                                            {day.weight}
-                                        </td>
-                                        <td className="border px-4 py-2">
-                                            {day.rest}
-                                        </td>
-                                    </tr>
-                            </tbody>
-                        </th>
-                    </tr>
-                    ))}
-                </tbody>
-            </table>
+          <table className="w-full text-left mt-4">
+            <tbody className="rounded-full text-[#161A1D]">
+              {days.map((day, index) => (
+                <tr key={index}>
+                  {/* LET "Order" AND "Exercise" CHANGE! 
+                        This is hard coded for UI purposes */}
+                  <th scope="col" className="px-1.5 py-1 border-solid border-2 border-black bg-gray-300">
+                    {alphabet[index]}
+                  </th>
+                  <th scope="col" className="px-1.5 py-1 border-solid border-2 border-black bg-gray-300">
+                    Movement {day.movement}
+                    <td scope="col" className="px-1.5 text-xs bg-gray-200">
+                      Equipment
+                    </td>
+                    <td scope="col" className="px-3 text-xs bg-gray-100">
+                      Sets
+                    </td>
+                    <td scope="col" className="px-3 text-xs bg-gray-200">
+                      Reps/Time
+                    </td>
+                    <td scope="col" className="px-3 text-xs bg-gray-100">
+                      % or RPE
+                    </td>
+                    <td scope="col" className="p-3 px-5 text-xs bg-gray-200">
+                      Rest
+                    </td>
+                    <tbody className="text-s rounded-full text-[#161A1D] bg-gray-100">
+                      <tr key={index}>
+                        <td className="border px-4 py-2">
+                          {day.equipment}
+                        </td>
+                        <td className="border px-4 py-2">
+                          {day.sets}
+                        </td>
+                        <td className="border px-4 py-2">
+                          {day.reps}
+                        </td>
+                        <td className="border px-4 py-2">
+                          {day.weight}
+                        </td>
+                        <td className="border px-4 py-2">
+                          {day.rest}
+                        </td>
+                      </tr>
+                    </tbody>
+                  </th>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         );
-    };
+      };
+      
 
     return (
         <div className="w-full h-full flex flex-col bottom-20">
@@ -121,7 +124,7 @@ export default function General() {
                                         </button>
                                     ))}
                                 </div>
-                                {getTable(eval(`${days.toLowerCase()}DayProgram`).filter(day => day))}
+                                {getTable(eval(`${days.toLowerCase()}DayProgram`).filter(day => day+1))}
                             </CardBack>
                         )}
                     </div>
