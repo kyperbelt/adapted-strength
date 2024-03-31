a35b95bdb7f84eaeb4914b4b5c70f82783f4b084
CaseyCubed <casey.paras@yahoo.com>
2024-03-31 12:03:34 -0700

    Touched up program days to toggle independently of each other]asd

--- frontend/adapted_strength-app/src/pages/program_pages/General.jsx
+++ frontend/adapted_strength-app/src/pages/program_pages/General.jsx
@@ -1,48 +1,40 @@
-/*
-Module: General.jsx
-Team: TeraBITE
-*/
-//import { Outlet, Link } from "react-router-dom";
-import { useState } from "react";
-import programFiller from "../../assets/blank_filler.png"
+import React, { useState } from "react";
 import { CardBack } from "../../components/Card";
-import { ProgramMamagement } from "../../pages/ProgramManagement"
 
-/*
-function ClientField() {
-    return (<input type="client" placeholder="Client" id="client" name="client" required className="w-4/5 border-b-4 p-0" />);
-}
-function SpecialtyField() {
-    return (<input type="specialty" placeholder="Specialty" id="specialty" name="specialty" required className="w-4/5 border-b-4 p-0" />);
-}
-*/
 export default function General() {
-    const [showProgram, setShowProgram] = useState({
+    const [showPrograms, setShowPrograms] = useState({
         fiveDay: false,
         fourDay: false,
-        threeDay: false,
-        days: {
-            fiveDay: Array(5).fill(false),
-            fourDay: Array(4).fill(false),
-            threeDay: Array(3).fill(false)
-        }
-    }
-    );
+        threeDay: false
+    });
+
+    const [fiveDayProgram, setFiveDayProgram] = useState(Array(5).fill(false));
+    const [fourDayProgram, setFourDayProgram] = useState(Array(4).fill(false));
+    const [threeDayProgram, setThreeDayProgram] = useState(Array(3).fill(false));
+
     const toggleProgram = (program) => {
-        setShowProgram(prevState => ({
+        setShowPrograms(prevState => ({
             ...prevState,
             [program]: !prevState[program]
         }));
     };
+
     const toggleDay = (program, index) => {
-        setShowProgram(prevState => ({
-            ...prevState,
-            days: {
-                ...prevState.days,
-                [program]: prevState.days[program].map((value, i) => i === index ? !value : value)
-            }
-        }));
+        switch (program) {
+            case "fiveDay":
+                setFiveDayProgram(prevState => prevState.map((day, i) => i === index ? !day : false));
+                break;
+            case "fourDay":
+                setFourDayProgram(prevState => prevState.map((day, i) => i === index ? !day : false));
+                break;
+            case "threeDay":
+                setThreeDayProgram(prevState => prevState.map((day, i) => i === index ? !day : false));
+                break;
+            default:
+                break;
+        }
     };
+
     return (
         <div className="w-full h-full flex flex-col bottom-20">
             <p className="bg-[#161A1D] text-white bottom-3 px-0 pt-8 pb-8">
@@ -54,17 +46,16 @@ export default function General() {
                 </h1>
                 {["Five", "Four", "Three"].map((days, index) => (
                     <div key={index}>
-                        <button onClick={() => toggleProgram(`${days.toLowerCase()}Day`)} className="w-full border-solid border-2 border-black uppercase rounded-full font-bold mt-2 px-0 pt-3 pb-3 bg-gray-200">
-                            {showProgram[`${days.toLowerCase()}Day`] ? `Close ${days}-Day Program` : `Show ${days}-Day`}
+                        <button onClick={() => toggleProgram(`${days.toLowerCase()}Day`)} className="w-full border-solid border-2 border-black uppercase rounded-full font-bold mt-1 mb-3 px-2 py-3 bg-gray-200">
+                            {showPrograms[`${days.toLowerCase()}Day`] ? `Close ${days}-Day Program` : `Show ${days}-Day`}
                         </button>
-                        {showProgram[`${days.toLowerCase()}Day`] && (
+                        {showPrograms[`${days.toLowerCase()}Day`] && (
                             <CardBack className="xl:w-11/12 xl:mx-auto w-full">
                                 <div className="overflow-x-auto">
-                                    {showProgram.days[`${days.toLowerCase()}Day`].map((day, dayIndex) => (
-                                        <button key={dayIndex} onClick={() => toggleDay(`${days.toLowerCase()}Day`, dayIndex)} className="w-500 border-solid border-2 border-black uppercase rounded-full font-bold p-3 bg-gray-200">
+                                    {eval(`${days.toLowerCase()}DayProgram`).map((day, dayIndex) => (
+                                        <button key={dayIndex} onClick={() => toggleDay(`${days.toLowerCase()}Day`, dayIndex)} className={`w-500 mr-1 border-solid border-2 border-black uppercase rounded-full font-bold p-2 ${day ? 'bg-[#f54242]' : 'bg-gray-200'}`}>
                                             {day ? 'Close' : `Day ${dayIndex + 1}`}
                                         </button>
-                                        
                                     ))}
                                 </div>
                             </CardBack>
@@ -73,6 +64,5 @@ export default function General() {
                 ))}
             </CardBack>
         </div>
-
     );
 }
