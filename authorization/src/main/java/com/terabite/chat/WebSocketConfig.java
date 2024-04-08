8163454087b3b03cc5b7c64f83fc9dbb61d2a8a2
Blake Danz <114424722+Buh-La-Kay@users.noreply.github.com>
2024-04-08 13:20:31 -0700

    Chat cleanup (#78)
    * Initial commit
    
    * Backend chat is actually working. Fixing minor stuff still
    
    * Chat should be fully good to go. Doing some final testing
    
    * Chat backend functionality in place.
    
    * Added workoutOrder field to RepCycle table so that client can indicate super sets.
    
    * Final changes before pr attempt

--- authorization/src/main/java/com/terabite/chat/WebSocketConfig.java
+++ authorization/src/main/java/com/terabite/chat/WebSocketConfig.java
@@ -3,10 +3,17 @@ package com.terabite.chat;
 import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.converter.DefaultContentTypeResolver;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -17,7 +24,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-    public static final String MESSAGE_PREFIX = "/v1/chat";
+    public static final String MESSAGE_PREFIX = "";
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
@@ -27,6 +34,29 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         // /user
         registry.setUserDestinationPrefix(MESSAGE_PREFIX + "/user");
     }
+
+    // @Override
+    // public void configureClientInboundChannel(ChannelRegistration registration){
+    //     registration.interceptors(new ChannelInterceptor() {
+    //         @Override
+    //         public Message<?> preSend(Message<?> message, MessageChannel channel) {
+    //             StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+
+    //             if(StompCommand.CONNECT.equals(accessor.getCommand())){
+    //                 System.out.println("Connect ");
+    //             } else if(StompCommand.SUBSCRIBE.equals(accessor.getCommand())){
+    //                 System.out.println("Subscribe ");
+    //                 System.out.println(String.format("\nMessage: %s\n Channel: %s\n", message, channel));
+    //             } else if(StompCommand.SEND.equals(accessor.getCommand())){
+    //                 System.out.println("Send message " );
+    //             } else if(StompCommand.DISCONNECT.equals(accessor.getCommand())){
+    //                 System.out.println("Exit ");
+    //             } else {
+    //             }
+    //             return message;
+    //         }
+    //     });
+    // }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
