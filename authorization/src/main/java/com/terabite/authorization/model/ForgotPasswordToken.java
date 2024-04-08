10f899a78e6002b009ceeaf4caabf48bb24e567b
Buh-La-Kay <114424722+Buh-La-Kay@users.noreply.github.com>
2024-04-08 12:51:35 -0700

    Getting started

---
+++ authorization/src/main/java/com/terabite/authorization/model/ForgotPasswordToken.java
@@ -0,0 +1,28 @@
+package com.terabite.authorization.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "forgot_password_table")
+public class ForgotPasswordToken {
+    private String token;
+    private LocalDateTime createdAt;
+
+    public String getToken() {
+        return token;
+    }
+    public void setToken(String token) {
+        this.token = token;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+
+}
