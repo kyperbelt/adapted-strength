93c9197d3f0cf15c0cfeea41eb0f111881c67bb9
Sanguine-Choleric <130211779+Sanguine-Choleric@users.noreply.github.com>
2024-04-14 19:36:59 -0700

    [ADAPTEDS-75] Removing old (wrong) movement note code

--- authorization/src/main/java/com/terabite/movement/repository/MovementNoteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.terabite.movement.repository;
-
-import com.terabite.movement.model.MovementNote;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovementNoteRepository extends JpaRepository<MovementNote, Integer> {
-}
