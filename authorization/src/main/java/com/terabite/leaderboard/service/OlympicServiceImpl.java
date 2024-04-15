2bd3c5a4a1640636022c93573b04ff6dcc1351e0
David Castrejon <119557047+davidjcastrejon@users.noreply.github.com>
2024-04-14 22:13:31 -0700

    [ADAPTEDS-147] Leaderboard (#80)
    * Added docker msql setup
    
    * Added table schema and data persistence within database
    
    * Remove docker_compose_mysql.yml
    
    * Leaderboard backend complete
    
    * Added front-end drop down menu's for searching records
    
    * Leaderboard front end display complete
    
    * Leaderboard addition of records functionality complete
    
    * Added deletion functionality to leaderboard
    
    * Added styling to add and delete buttons
    
    * Fixed adding records bug

--- authorization/src/main/java/com/terabite/leaderboard/service/OlympicServiceImpl.java
+++ authorization/src/main/java/com/terabite/leaderboard/service/OlympicServiceImpl.java
@@ -1,15 +1,22 @@
 package com.terabite.leaderboard.service;
 
 import com.terabite.leaderboard.model.Olympic;
+import com.terabite.leaderboard.model.Powerlifting;
 import com.terabite.leaderboard.repository.OlympicRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class OlympicServiceImpl implements OlympicService {
 
+    private final OlympicRepository olympicRepository;
+
     @Autowired
-    private OlympicRepository olympicRepository;
+    public OlympicServiceImpl(OlympicRepository olympicRepository) {
+        this.olympicRepository = olympicRepository;
+    }
 
     @Override
     public Olympic addOlympicEntry(Olympic olympic) {
@@ -20,5 +27,17 @@ public class OlympicServiceImpl implements OlympicService {
     @Override
     public void deleteOlympicEntry(Long id) {
         olympicRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Olympic> getTop10MaleAthletesByWeightClass(String weightClass) {
+        return olympicRepository.findTop10ByWeightClassAndGenderOrderByTotalDesc(weightClass, 'M');
+    }
+
+    @Override
+    public List<Olympic> getTop10FemaleAthletesByWeightClass(String weightClass) {
+        // Implement the logic to fetch top 10 female athletes by weight class
+        // You can use the repository method here
+        return olympicRepository.findTop10ByWeightClassAndGenderOrderByTotalDesc(weightClass, 'F');
     }
 }
--- authorization/src/main/java/com/terabite/leaderboard/service/PowerliftingService.java
+++ authorization/src/main/java/com/terabite/leaderboard/service/PowerliftingService.java
@@ -2,7 +2,12 @@ package com.terabite.leaderboard.service;
 
 import com.terabite.leaderboard.model.Powerlifting;
 
+import java.util.List;
+
 public interface PowerliftingService {
     Powerlifting addPowerliftingEntry(Powerlifting powerlifting);
     void deletePowerliftingEntry(Long id);
+    List<Powerlifting> getTop10MalePowerliftersByWeightClass(String weightClass);
+
+    List<Powerlifting> getTop10FemalePowerliftersByWeightClass(String weightClass);
 }
