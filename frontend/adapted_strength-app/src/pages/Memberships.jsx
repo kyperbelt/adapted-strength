5747a2034d7e9d6cd476ac99e667de108980fedb
David Castrejon <david.j.castrejon@gmail.com>
2024-04-14 22:12:50 -0700

    Users may change their subscription tier

--- frontend/adapted_strength-app/src/pages/Memberships.jsx
+++ frontend/adapted_strength-app/src/pages/Memberships.jsx
@@ -11,6 +11,29 @@ function AdaptedStrengthLogo() {
   );
 }
 
+function DropDownMenu({ onSubmit }) {
+  const handleSelectChange = (e) => {
+    const selectedValue = e.target.value;
+    onSubmit(selectedValue);
+  };
+
+  return (
+    <div className="text-center border-b-4 p-1">
+      <label htmlFor="subscriptionTier">Membership: </label>
+      <select
+        name="subscriptionTier"
+        id="subscriptionTier"
+        onChange={handleSelectChange}
+      >
+        <option value="">--Select Tier--</option>
+        <option value="BASE_CLIENT">Base Client - Program Only</option>
+        <option value="GENERAL_CLIENT">General Client</option>
+        <option value="SPECIFIC_CLIENT">Specific Client</option>
+      </select>
+    </div>
+  );
+}
+
 function SubscriptionField({...props}) {
     if (props.tier === 'BASE_CLIENT') {
         return (<div> <p>Base Client</p></div>);
@@ -22,12 +45,14 @@ function SubscriptionField({...props}) {
 }
 
 function ExpirationField({...props}) {
-    return (<div> (Need to implement) {props.expirationDate}</div>);
+    return (<div> (Need to implement) {props.expiration_date}</div>);
 }
 
 export default function Memberships() {
     const [isLoading, setIsLoading] = useState(true);
     const [profileInfo, setProfileInfo] = useState([]);
+    const [selectedTier, setSelectedTier] = useState(profileInfo.subscriptionTier || '');
+
   
     useEffect(() => {
       setIsLoading(true);
@@ -48,6 +73,21 @@ export default function Memberships() {
         });
     }, []);
   
+    const onSubmit = async (selectedValue) => {
+        console.log("Selected Value:", selectedValue);
+        const data = {
+          subscriptionTier: selectedValue,
+        };
+        console.log("Data:", data);
+        await UserApi.updateSubscription(data).then((response) => {
+          if (response.status === 200) {
+            console.log("Subscription updated successfully");
+            // Update the selected tier in the state
+            setSelectedTier(selectedValue);
+          }
+        });
+      };
+  
     if (isLoading) {
       return <div>{"Loading..."}</div>;
     }
@@ -57,7 +97,25 @@ export default function Memberships() {
             <div className="h-56 bg-header-background1">
                 <AdaptedStrengthLogo />
             </div>
+
+            <br></br>
             
+
+            <div className='container mx-auto w-2/6 content-center text-center px-2 bg-blue-200 text-black rounded-md'>
+                    <h2 className='font-bold'>YOUR SUBSCRIPTION TIER</h2>
+                    <p><SubscriptionField tier={selectedTier} /></p>
+
+                    <h2 className='font-bold'>PAYMENT DATE</h2>
+                    <p><ExpirationField exp={profileInfo.expiration_date}/></p>
+                    <br></br>
+
+                    <h2 className='font-bold'>UPGRADE OR SWITCH MEMBERSHIPS HERE</h2>
+                    <DropDownMenu onSubmit={onSubmit} />
+
+                </div>
+
+            <br></br>
+
             <div>
                 <p className='font-bold text-lg'>Adapted Strength (A.S.) Memberships</p>
                 <h3>Book a Consultation for one-time FREE Access!</h3>   
@@ -98,14 +156,6 @@ export default function Memberships() {
                     <p className='font-light'>(INTENDED FOR THOSE SEEKING IN-DEPTH COACHING)</p>
                     <p>* ONCE EXPERIENCED IN ADAPTED STRENGTH METHODOLOGIES, ATHLETES WILL BE GIVEN "GENERAL CLIENT" RATE</p>
                 </div>      
-            </div>
-            <br></br>
-            <div className='container mx-auto w-2/6 content-center text-center px-2 bg-blue-200 text-black rounded-md'>
-              <h2 className='font-bold'>YOUR SUBSCRIPTION TIER</h2>
-              <p><SubscriptionField tier={profileInfo.subscriptionTier} /></p>
-
-              <h2 className='font-bold'>PAYMENT DATE</h2>
-              <p><ExpirationField exp={profileInfo.expirationDate}/></p>
             </div>
             <br></br>
             <Footer />
