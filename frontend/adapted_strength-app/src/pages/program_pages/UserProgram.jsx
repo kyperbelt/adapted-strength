362ef981f24328a5efa4d6cc325c6c7fe9623b6a
CaseyCubed <casey.paras@yahoo.com>
2024-04-28 20:16:28 -0700

    Premerge save:

--- frontend/adapted_strength-app/src/pages/program_pages/UserProgram.jsx
+++ frontend/adapted_strength-app/src/pages/program_pages/UserProgram.jsx
@@ -4,12 +4,15 @@ Module: General.jsx
 import React, { useEffect, useState } from "react";
 import { CardBack } from "../../components/Card";
 import { ProgrammingApi } from "../../api/ProgrammingApi";
+import { UserApi } from "../../api/UserApi";
 
 export default function General() {
 
   const [program, setProgram] = useState(null);
+  const [userProgram, setUserProgram] = useState(null);
   const [currentWeek, setCurrentWeek] = useState(0);
   const [currentDays, setCurrentDays] = useState(null);
+
 
   useEffect(() => {
     ProgrammingApi.getProgram(1)
@@ -26,8 +29,33 @@ export default function General() {
       });
   }, []);
 
+  useEffect(() => {
+    UserApi.getUserProgramming()
+    .then((data) => {
+      const p = data;
+      setUserProgram(p);
+    });
+}, []);
+
+
   if (!program) {
     return (<>Loading</>);
+  }
+
+  if (!userProgram) {
+    return (
+      <div className="w-full h-full flex flex-col bottom-20">
+        <p className="bg-[#161A1D] text-white flex justify-center bottom-3 px-0 pt-8 pb-8">
+          Your Program: NONE
+        </p>
+        <h1 className="font-bold flex justify-center">
+          Looks like you don't  have a program yet!
+        </h1>
+        <h2 className="font-bold flex justify-center">
+          Contact Coach Alex for help!
+        </h2>
+      </div>
+    );
   }
 
   const getTable = (day) => {
@@ -63,23 +91,23 @@ export default function General() {
           program.name
         }
       </p>
-        <h1 className="font-bold flex justify-center uppercase">
-          {program && program.weeks[0].name}:
-        </h1>
-        <div>
-          <CardBack className="xl:w-11/12 xl:mx-auto w-full h-full">
-            <div className="overflow-x-auto flex flex-col text-left justify-center space-y-2 h-full">
-              {currentDays.map((day, dayIndex) => (
-                <>
-                  <button onClick={() => onDayClick(day)} className={`w-500 text-left pl-4 mx-4 border-solid border-2 border-black uppercase rounded-full font-bold p-2 ${day.toggled ? 'bg-[#f54242]' : 'bg-gray-200'}`}>
-                    {day.toggled ? 'Close' : `Day ${dayIndex + 1}: ${day.name}`}
-                  </button>
-                  {day.toggled && getTable(day)}
-                </>
-              ))}
-            </div>
-          </CardBack>
-        </div>
+      <h1 className="font-bold flex justify-center uppercase">
+        {program && program.weeks[0].name}:
+      </h1>
+      <div>
+        <CardBack className="xl:w-11/12 xl:mx-auto w-full h-full">
+          <div className="overflow-x-auto flex flex-col text-left justify-center space-y-2 h-full">
+            {currentDays.map((day, dayIndex) => (
+              <>
+                <button onClick={() => onDayClick(day)} className={`w-500 text-left pl-4 mx-4 border-solid border-2 border-black uppercase rounded-full font-bold p-2 ${day.toggled ? 'bg-[#f54242]' : 'bg-gray-200'}`}>
+                  {day.toggled ? 'Close' : `Day ${dayIndex + 1}: ${day.name}`}
+                </button>
+                {day.toggled && getTable(day)}
+              </>
+            ))}
+          </div>
+        </CardBack>
+      </div>
     </div>
   );
 }
