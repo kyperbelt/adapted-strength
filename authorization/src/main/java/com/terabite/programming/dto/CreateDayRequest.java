0b01ad6796d81eacc95bf045618ed1021d876128
CaseyCubed <casey.paras@yahoo.com>
2024-04-28 23:18:19 -0700

    Merge Fix

--- authorization/src/main/java/com/terabite/programming/dto/CreateDayRequest.java
+++ authorization/src/main/java/com/terabite/programming/dto/CreateDayRequest.java
@@ -1,16 +1,7 @@
 package com.terabite.programming.dto;
-<<<<<<< HEAD
 import java.io.Serializable;
 /**
  * CreateDayRequest
  */
 public record CreateDayRequest(String dayName, String dayDescription) implements Serializable{
-=======
-
-
-/**
- * CreateDayRequest
- */
-public record CreateDayRequest(String dayName, String dayDescription) {
->>>>>>> program_management_redo
 }
