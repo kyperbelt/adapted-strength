45288633374744cf5fe26655d89b21afe42157d3
Jonathan Camarena <jonathancamarenacamcho@gmail.com>
2024-05-01 21:28:27 -0700

    Added return button to edit profile & added edit sub to prodile

--- frontend/adapted_strength-app/src/components/Icons.jsx
+++ frontend/adapted_strength-app/src/components/Icons.jsx
@@ -1,7 +1,7 @@
 export function PencilIcon({ className, ...props }) {
   return (
     <svg
-      className="w-6 h-6 text-gray-800 dark:text-white"
+      className={`w-6 h-6${className}`}
       aria-hidden="true"
       xmlns="http://www.w3.org/2000/svg"
       width="24"
