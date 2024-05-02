ffc570a806127f705b20bb6ee9d9b7ea357ea3cd
Jonathan Camarena <jonathancamarenacamcho@gmail.com>
2024-05-02 00:12:46 -0700

    Handoff to CASEMeister

--- frontend/adapted_strength-app/src/pages/program_pages/UserProgram.jsx
+++ frontend/adapted_strength-app/src/pages/program_pages/UserProgram.jsx
@@ -5,163 +5,193 @@ import React, { useEffect, useState } from "react";
 import { CardBack } from "../../components/Card";
 import { ProgrammingApi } from "../../api/ProgrammingApi";
 import { UserApi } from "../../api/UserApi";
+import { PageContainer2 } from "../../components/PageContainer";
 
 export default function General() {
-
-  const [program, setProgram] = useState(null);
-  const [userProgram, setUserProgram] = useState(null);
-  const [currentWeek, setCurrentWeek] = useState(0);
-  const [currentDays, setCurrentDays] = useState(null);
-
-
-  useEffect(() => {
-    ProgrammingApi.getProgram(1)
-      .then((data) => {
-        const p = data;
-        setProgram(p);
-        setCurrentWeek(p.weeks[0]);
-        setCurrentDays(p.weeks[0].days.map((day) => {
-          return {
-            ...day,
-            toggled: false
-          };
-        }));
-      });
-  }, []);
+  const [userProgrammming, setUserProgramming] = useState(null);
 
   useEffect(() => {
     UserApi.getUserProgramming()
       .then((data) => {
-        const p = data;
-        setUserProgram(p);
+        return data.subscribed_programs;
+      })
+      .then((subscribed_programs) => {
+        let programPromises = subscribed_programs.map((program) => {
+          return ProgrammingApi.getProgram(program.assignedProgramId).then(
+            (userProgram) => {
+              return {
+                startDate: program.startDate,
+                startWeek: program.startWeek,
+                currentWeek: ProgrammingApi.getCurrentWeek(
+                  program.startDate,
+                  program.startWeek
+                ),
+                userProgram: userProgram,
+              };
+            }
+          );
+        });
+
+        return Promise.all(programPromises);
+      })
+      .then((programs) => {
+        let userProgramming = programs;
+        console.log("User Programming: ", userProgramming);
+        setUserProgramming(userProgramming);
       });
   }, []);
 
+  // TO GET THE PROGRAMMING use userProgramming to get the programming for this user. 
+  
+  // Steps 
+  // Check that userProgramming is not null
+  // If it is null, return a loading screen
+  // If it is not null, render the user programming 
+  // The userProgramming Structure is an Array of all the programs assigned to that user. 
+  //
+  // Each program contains the following fields:
+  // startDate: The date the program started
+  // startWeek: The week the program started
+  // currentWeek: The current week of the program
+  // userProgram: The actual program object that contains the program information 
+  //
+  // To access the first program for example you would do userProgramming[0].userProgram
+  //
+  // to get the current week for the first program you would do userProgramming[0].weeks[userProgramming[0].currentWeek]
+  //
+  // to get all the current weeks you would do const weeks = userProgramming.map(program => program.weeks[program.currentWeek])
 
-  if (!program) {
-    return (<>Loading</>);
-  }
 
-  if (!userProgram) {
+  // if (!program) {
+  //   return (<>Loading</>);
+  // }
+
+  if (!userProgrammming) {
     return (
-      <div className="w-full h-full flex flex-col bottom-20">
+      <PageContainer2>
         <p className="bg-[#161A1D] text-white flex justify-center bottom-3 px-0 pt-8 pb-8">
           Your Program: NONE
         </p>
         <h1 className="font-bold flex justify-center">
-          Looks like you don't  have a program yet!
+          Looks like you don't have a program yet!
         </h1>
         <h2 className="font-bold flex justify-center">
           Contact Coach Alex for help!
         </h2>
-      </div>
+      </PageContainer2>
     );
   }
 
-  const getTable = (day) => {
-    return (
-      <table className="w-full text-left mt-4 min-h-full">
-        <tbody className="text-[#161A1D]">
-          {program &&
-            day.repCycles.map((repCycle) => (
-              <RepCycle repCycle={repCycle} />
-            ))}
-        </tbody>
-      </table>
-    );
-  };
+  // const getTable = (day) => {
+  //   return (
+  //     <table className="w-full text-left mt-4 min-h-full">
+  //       <tbody className="text-[#161A1D]">
+  //         {program &&
+  //           day.repCycles.map((repCycle) => (
+  //             <RepCycle repCycle={repCycle} />
+  //           ))}
+  //       </tbody>
+  //     </table>
+  //   );
+  // };
 
-  const onDayClick = (day) => {
-    const newDays = currentDays.map((day1) => {
-      if (day1.dayId == day.dayId) {
-        return {
-          ...day1,
-          toggled: !day1.toggled
-        };
-      }
-      return { ...day1, toggled: false };
-    })
-    setCurrentDays(newDays);
-  }
-  return (
-    <div className="w-full h-full flex flex-col bottom-20">
-      <p className="bg-[#161A1D] text-white flex justify-center bottom-3 px-0 pt-8 pb-8">
-        Your Program: {
-          program &&
-          program.name
-        }
-      </p>
-      <h1 className="font-bold flex justify-center uppercase">
-        {program && program.weeks[0].name}:
-      </h1>
-      <div>
-        <CardBack className="xl:w-11/12 xl:mx-auto w-full h-full">
-          <div className="overflow-x-auto flex flex-col text-left justify-center space-y-2 h-full">
-            {currentDays.map((day, dayIndex) => (
-              <>
-                <button onClick={() => onDayClick(day)} className={`w-500 text-left pl-4 mx-4 border-solid border-2 border-black uppercase rounded-full font-bold p-2 ${day.toggled ? 'bg-[#f54242]' : 'bg-gray-200'}`}>
-                  {day.toggled ? 'Close' : `Day ${dayIndex + 1}: ${day.name}`}
-                </button>
-                {day.toggled && getTable(day)}
-              </>
-            ))}
-          </div>
-        </CardBack>
-      </div>
-    </div>
-  );
+  return <PageContainer2></PageContainer2>;
 }
-
+// <p className="bg-[#161A1D] text-white flex justify-center bottom-3 px-0 pt-8 pb-8">
+//   Your Program: {
+//     program &&
+//     program.name
+//   }
+// </p>
+// <h1 className="font-bold flex justify-center uppercase">
+//   {program && program.weeks[0].name}:
+// </h1>
+// <div>
+//   <CardBack className="xl:w-11/12 xl:mx-auto w-full h-full">
+//     <div className="overflow-x-auto flex flex-col text-left justify-center space-y-2 h-full">
+//       {currentDays.map((day, dayIndex) => (
+//         <>
+//           <button onClick={() => onDayClick(day)} className={`w-500 text-left pl-4 mx-4 border-solid border-2 border-black uppercase rounded-full font-bold p-2 ${day.toggled ? 'bg-[#f54242]' : 'bg-gray-200'}`}>
+//             {day.toggled ? 'Close' : `Day ${dayIndex + 1}: ${day.name}`}
+//           </button>
+//           {day.toggled && getTable(day)}
+//         </>
+//       ))}
+//     </div>
+//   </CardBack>
+// </div>
+//
 
 function RepCycle({ repCycle }) {
   return (
-  <tr key={repCycle.repCycleId} className="text-center m-auto">
-    <td className="m-auto px-2 border-solid border-2 border-black bg-gray-300 w-auto font-bold">
-      {repCycle.workoutOrder}
-    </td>
-    <th scope="col" className="p-1.5 w-full border-solid border-2 border-black bg-gray-300">
-      {repCycle.name}
-      <div className="flex justify-center">
-        <table className="w-full">
-          <tbody>
-            <tr>
-              <td scope="col" className="px-1 w-auto text-xs bg-gray-200 text-center">
-                Equipment
-              </td>
-              <td scope="col" className="px-1 w-auto text-xs bg-gray-100 text-center">
-                Sets
-              </td>
-              <td scope="col" className="px-1 w-auto text-xs bg-gray-200 text-center">
-                Reps/Time
-              </td>
-              <td scope="col" className="px-1 w-auto text-xs bg-gray-100 text-center">
-                % / RPE
-              </td>
-              <td scope="col" className="p-1.5 w-auto text-xs bg-gray-200 text-center">
-                Rest
-              </td>
-            </tr>
-            <tr key={repCycle.repCycleId} className="text-s text-[#161A1D] bg-gray-100">
-              <td className="border text-center bg-gray-200 px-1.5 py-2">
-                {repCycle.equipment}
-              </td>
-              <td className="border text-center px-1.5 py-2">
-                {repCycle.numSets}
-              </td>
-              <td className="border text-center bg-gray-200 px-1.5 py-2">
-                {repCycle.numReps}
-              </td>
-              <td className="border text-center px-1.5 py-2">
-                {repCycle.weight}
-              </td>
-              <td className="border text-center bg-gray-200 px-1.5 py-2">
-                {repCycle.restTime}
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </th>
-  </tr>
-);
+    <tr key={repCycle.repCycleId} className="text-center m-auto">
+      <td className="m-auto px-2 border-solid border-2 border-black bg-gray-300 w-auto font-bold">
+        {repCycle.workoutOrder}
+      </td>
+      <th
+        scope="col"
+        className="p-1.5 w-full border-solid border-2 border-black bg-gray-300"
+      >
+        {repCycle.name}
+        <div className="flex justify-center">
+          <table className="w-full">
+            <tbody>
+              <tr>
+                <td
+                  scope="col"
+                  className="px-1 w-auto text-xs bg-gray-200 text-center"
+                >
+                  Equipment
+                </td>
+                <td
+                  scope="col"
+                  className="px-1 w-auto text-xs bg-gray-100 text-center"
+                >
+                  Sets
+                </td>
+                <td
+                  scope="col"
+                  className="px-1 w-auto text-xs bg-gray-200 text-center"
+                >
+                  Reps/Time
+                </td>
+                <td
+                  scope="col"
+                  className="px-1 w-auto text-xs bg-gray-100 text-center"
+                >
+                  % / RPE
+                </td>
+                <td
+                  scope="col"
+                  className="p-1.5 w-auto text-xs bg-gray-200 text-center"
+                >
+                  Rest
+                </td>
+              </tr>
+              <tr
+                key={repCycle.repCycleId}
+                className="text-s text-[#161A1D] bg-gray-100"
+              >
+                <td className="border text-center bg-gray-200 px-1.5 py-2">
+                  {repCycle.equipment}
+                </td>
+                <td className="border text-center px-1.5 py-2">
+                  {repCycle.numSets}
+                </td>
+                <td className="border text-center bg-gray-200 px-1.5 py-2">
+                  {repCycle.numReps}
+                </td>
+                <td className="border text-center px-1.5 py-2">
+                  {repCycle.weight}
+                </td>
+                <td className="border text-center bg-gray-200 px-1.5 py-2">
+                  {repCycle.restTime}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </th>
+    </tr>
+  );
 }
